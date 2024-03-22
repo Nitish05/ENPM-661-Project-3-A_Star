a4f8b02417942a0e35b53452761b5885803825ab
Nitish05 <rrnitish@gmail.com>
2024-03-21 21:06:31 -0400

    skeleton readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # ENPM-661-Project-3-A_Star
+
+This repository contains the code for Project 3 of the ENPM 661 course. The project focuses on implementing the A* algorithm for path planning in a given environment.
+
+## Installation
+
+To use this code, you need to have Python installed on your system. You can download Python from the official website: [Python.org](https://www.python.org/).
+
+## Usage
+
+1. Clone the repository to your local machine:
+
+    ```bash
+    git clone https://github.com/your-username/ENPM-661-Project-3-A_Star.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd ENPM-661-Project-3-A_Star
+    ```
+
+3. Run the main script:
+
+    ```bash
+    python main.py
+    ```
+
+4. Follow the on-screen instructions to input the start and goal positions, and the program will generate the optimal path using the A* algorithm.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
+
+## Acknowledgements
+
+Special thanks to the ENPM 661 course instructors for providing the project requirements and guidance.
--- test.py
+++ test.py
@@ -1,86 +1,33 @@
+# This script was used to test the arrowedLine function in OpenCV
+
 import cv2
 import numpy as np
-from queue import PriorityQueue
-import time
-import random
 
-canvas_height = 501
-canvas_width = 1201
-free_space_color = (255, 255, 255)
-obstacle_color = (0, 0, 0)
-canvas = np.ones((canvas_height, canvas_width, 3), dtype="uint8") * 255  
+# Create a blank image, +1 in dimensions for boundary conditions
+image = np.zeros((500, 500, 3), dtype="uint8")
 
-def draw_rectangle(center, width, height, color):
-    top_left = (int(center[0] - width/2), int(center[1] - height/2))
-    bottom_right = (int(center[0] + width/2), int(center[1] + height/2))
-    cv2.rectangle(canvas, top_left, bottom_right, color, -1)
-
-clearance = [
-    ((137.5, 200), 85, 410),
-    ((312.3, 300), 85, 410),
-    ((1000, 87.5), 210, 85),
-    ((1000, 412.5), 210, 85),
-    ((1060, 250), 90, 240),
-]
-
-obstacle = [
-    ((137.5, 200), 75, 400),
-    ((312.3, 300), 75, 400),
-    ((1000, 87.5), 200, 75),
-    ((1000, 412.5), 200, 75),
-    ((1060, 250),80, 250),
-]
-
-for center, width, height in clearance:
-    draw_rectangle(center, width, height, (127, 127, 127)) 
-
-for center, width, height in obstacle:
-    draw_rectangle(center, width, height, (0, 0, 0))
-
-center = (650, 250)
-side_length = 150
-radius_c = (side_length / (2 * np.sin(np.pi / 6))) + 5
-
-hex_c = []
-for i in range(6):
-    x_c = int(center[0] + radius_c * np.cos(i * 2 * np.pi / 6))
-    y_c = int(center[1] + radius_c * np.sin(i * 2 * np.pi / 6))
-    hex_c.append((x_c, y_c))
-    
-radius = (side_length / (2 * np.sin(np.pi / 6)))
-hex = []
-for j in range(6):
-    x = int(center[0] + radius * np.cos(j * 2 * np.pi / 6))
-    y = int(center[1] + radius * np.sin(j * 2 * np.pi / 6))
-    hex.append((x, y))
-
-pts_c = np.array(hex_c, np.int32)
-pts_c = pts_c.reshape((-1, 1, 2))
-color_c = (127, 127, 127)
-pts = np.array(hex, np.int32)
-pts = pts.reshape((-1, 1, 2))
-color = (0, 0, 0)
+# Define the starting and ending points of the vector
+start_point = (100, 100)  # (x1, y1)
+# end_point = (400, 400)    # (x2, y2)
 
 
-cv2.fillPoly(canvas, [pts_c], color_c)
-cv2.fillPoly(canvas, [pts], color)
+color = (255, 0, 0)  # Blue color in BGR
+thickness = 2
 
-x =650
-y = 250
-y = abs(canvas_height - y)
-w = 10
-l = 10
-theta = 30
-theta = np.deg2rad(theta)
-canvas[y,x] = (0,0,255)
+x = 100
+y = 100
+while True:
+    angle = input("Enter the angle of the vector: ")
+    angle = int(angle)
+    angle = np.radians(angle)
 
-direction = [ -2, -1, 0, 1, 2]
+    # Draw the vector as an arrowed line
+    xe = x + 50 * np.cos(angle)
+    ye = y - 50 * np.sin(angle)
+    end_point = (int(xe), int(ye))
+    cv2.arrowedLine(image, start_point, end_point, color, thickness)
+    cv2.imshow('Vector', image)
+    cv2.waitKey(0)
+    image = np.zeros((500, 500, 3), dtype="uint8")
+    cv2.destroyAllWindows()
 
-for i in  direction:
-    X = x + l*np.sin(theta * i)
-    Y = y + l*np.cos(theta * i)
-    Y = abs(canvas_height - Y)
-    cv2.line(canvas, (x, y), (int(X), int(Y)), (0, 0, 255), 2)
-
-cv2.imshow("Canvas", canvas)
-cv2.waitKey(0)
